5d96117ae9db3b02fa11c3057af3825041d6632c
rmuehe <rmuehe@gmail.com>
2021-11-19 19:46:53 -0800

    Update the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_18_022157) do
+ActiveRecord::Schema.define(version: 2021_11_20_033313) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
 end
