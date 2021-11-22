23c4bab3902e3cc5c25ba7be52975ad5d5ea9526
rmuehe <rmuehe@gmail.com>
2021-11-21 19:13:27 -0800

    Update the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_20_033313) do
+ActiveRecord::Schema.define(version: 2021_11_22_025612) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
