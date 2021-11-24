fffdb9c5962a8ed0c7fff5ac3b2bc3de6b1ff564
rmuehe <rmuehe@gmail.com>
2021-11-23 21:56:05 -0800

    Refactor pages_controller with before_action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
+    # before_action :set_page, except: [:index,:new, :create]
+    before_action :set_page, only: [:show, :edit, :update, :destroy]
+    
     def index
         @pages = Page.all
     end
 
     def show
-        @page = Page.find(params[:id])
+       # @page = Page.find(params[:id])
     end
 
     def new
@@ -18,17 +21,17 @@ class PagesController < ApplicationController
     end
 
     def edit
-        @page = Page.find(params[:id])
+        # @page = Page.find(params[:id])
     end
 
     def update
-        @page = Page.find(params[:id])
+        # @page = Page.find(params[:id])
         @page.update(page_params)
         redirect_to @page
     end
 
     def destroy
-        @page = Page.find(params[:id])
+        # @page = Page.find(params[:id])
         @page.destroy
         redirect_to pages_path
     end
@@ -37,5 +40,9 @@ class PagesController < ApplicationController
 
         def page_params
             params.require(:page).permit(:title, :body, :slug)
+        end
+
+        def set_page
+            @page = Page.find(params[:id])
         end
 end
