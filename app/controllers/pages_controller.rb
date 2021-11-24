c8eef62f5ebffa261d15444291e67b725f89c27b
rmuehe <rmuehe@gmail.com>
2021-11-23 19:43:19 -0800

    Make controller action to create pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,6 +12,9 @@ class PagesController < ApplicationController
     end
 
     def create
-        render plain: params.to_json
+        page_params = params.require(:page).permit(:title, :body, :slug)
+        @page = Page.new(page_params)
+        @page.save
+        redirect_to @page
     end
 end
