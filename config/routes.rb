0773c5d2adad6a27f5606b2da411eb8a07f53d4e
rmuehe <rmuehe@gmail.com>
2021-11-23 20:57:46 -0800

    Add patch route for pages controller update action

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
   get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  patch '/pages/:id', to: 'pages#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
