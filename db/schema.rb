ef7d13c8d3142e40cf9445c68985471f50527c65
rmuehe <rmuehe@gmail.com>
2021-11-27 17:24:17 -0800

    Generate Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_025612) do
+ActiveRecord::Schema.define(version: 2021_11_28_012259) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
