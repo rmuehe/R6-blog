31e0830474906bed4d2770647962711043e8c351
rmuehe <rmuehe@gmail.com>
2021-11-27 17:43:29 -0800

    Add post_id column to comments table & index it

--- db/migrate/20211128013419_add_post_to_comments.rb
+++ db/migrate/20211128013419_add_post_to_comments.rb
@@ -1,3 +1,5 @@
+# result following command:
+# => bin/rails g migration AddPostToComments post_id:integer:index
 class AddPostToComments < ActiveRecord::Migration[6.1]
   def change
     add_column :comments, :post_id, :integer
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_012259) do
+ActiveRecord::Schema.define(version: 2021_11_28_013419) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "pages", force: :cascade do |t|
