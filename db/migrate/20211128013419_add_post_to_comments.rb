610491addd4fbb5701046dc04f1059cd185870c7
rmuehe <rmuehe@gmail.com>
2021-11-28 18:11:40 -0800

    Rollback DB & remove migration

--- db/migrate/20211128013419_add_post_to_comments.rb
+++
@@ -1,8 +0,0 @@
-# result following command:
-# => bin/rails g migration AddPostToComments post_id:integer:index
-class AddPostToComments < ActiveRecord::Migration[6.1]
-  def change
-    add_column :comments, :post_id, :integer
-    add_index :comments, :post_id
-  end
-end
