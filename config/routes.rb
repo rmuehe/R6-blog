ba4a430b40615f30054634958adcee4584b0b9bf
rmuehe <rmuehe@gmail.com>
2021-11-30 18:10:00 -0800

    Add comments routes manually

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   resources :posts
+  
+  get     '/posts/:post_id/comments',           to: 'comments#index', as: 'post_comments'
+  post    '/posts/:post_id/comments',           to: 'comments#create'
+  get     '/posts/:post_id/comments/new',       to: 'comments#new',   as: 'new_post_comment'  
+  get     '/posts/:post_id/comments/:id',       to: 'comments#show',  as: 'post_comment'
+  get     '/posts/:post_id/comments/:id/edit',  to: 'comments#edit',  as: 'edit_post_comment'
+  patch   '/posts/:post_id/comments/:id',       to: 'comments#update'
+  delete  '/posts/:post_id/comments/:id',       to: 'comments#destroy'
+
   resources :pages
   # get '/pages', to: 'pages#index'
   # post '/pages', to: 'pages#create'
