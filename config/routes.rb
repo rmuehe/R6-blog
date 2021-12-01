c61c61120e306aa491b8c2ec5699945ca50a655c
rmuehe <rmuehe@gmail.com>
2021-11-30 18:17:57 -0800

    Nest comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments 
+    # get     '/comments',           to: 'comments#index', as: 'comments'
+    # post    '/comments',           to: 'comments#create'
+    # get     '/comments/new',       to: 'comments#new',   as: 'new_comment'  
+    # get     '/comments/:id',       to: 'comments#show',  as: 'comment'
+    # get     '/comments/:id/edit',  to: 'comments#edit',  as: 'edit_comment'
+    # patch   '/comments/:id',       to: 'comments#update'
+    # delete  '/comments/:id',       to: 'comments#destroy'
+  end  
   
-  get     '/posts/:post_id/comments',           to: 'comments#index', as: 'post_comments'
-  post    '/posts/:post_id/comments',           to: 'comments#create'
-  get     '/posts/:post_id/comments/new',       to: 'comments#new',   as: 'new_post_comment'  
-  get     '/posts/:post_id/comments/:id',       to: 'comments#show',  as: 'post_comment'
-  get     '/posts/:post_id/comments/:id/edit',  to: 'comments#edit',  as: 'edit_post_comment'
-  patch   '/posts/:post_id/comments/:id',       to: 'comments#update'
-  delete  '/posts/:post_id/comments/:id',       to: 'comments#destroy'
 
   resources :pages
   # get '/pages', to: 'pages#index'
